5a889a26d2bee95847efbe5f34d945272320f118
JMautone <nachomautone@gmail.com>
2024-07-16 22:26:22 -0300

    Fix health endpoint

--- src/main/java/uy/edu/ort/devops/productsserviceexample/endpoints/ProductsEndpoint.java
+++ src/main/java/uy/edu/ort/devops/productsserviceexample/endpoints/ProductsEndpoint.java
@@ -1,15 +1,17 @@
 package uy.edu.ort.devops.productsserviceexample.endpoints;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import uy.edu.ort.devops.productsserviceexample.domain.Product;
 import uy.edu.ort.devops.productsserviceexample.logic.ProductsLogic;
-
-import java.util.Collection;
 
 @RestController
 @RequestMapping("/products")
@@ -35,7 +37,7 @@ public class ProductsEndpoint {
 
     @GetMapping(path = "/health", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> healthCheck() {
-        return ResponseEntity.ok("{"status":"UP"}");
+        return ResponseEntity.ok("{\"status\":\"UP\"}");
     }
 
 }
